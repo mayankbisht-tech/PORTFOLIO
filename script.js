5bab86c3fad714730f5ed639512c7a406ad25e52
mayankbisht-tech <mayankbisht1107@gmail.com>
2024-11-10 18:08:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSection(sectionId) {
+    document.querySelectorAll('.section').forEach(section => {
+        section.style.display = 'none';
+    });
+    document.getElementById(sectionId).style.display = 'block';
+}
+
+// Set the default section to display
+window.onload = () => {
+    showSection('home');
+};
